ae24740b6b07337ffed4613d166f371af92246cb
Ben Bass <ben@benbass.com>
2013-11-11 07:31:22 -0500

    Time Machine update
    Changed verbiage and improved logging.  Added preflight.

--- Time Machine Schedule/Scripts - trmacs/timemachine.sh
+++ Time Machine Schedule/Scripts - trmacs/timemachine.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 #checks to see if home.benbass.com resolves to 10.0.240.10.  If not, skip running time machine.
 
+log="/Library/Logs/com.trmacs/timemachine.log"
+err_log="/Library/Logs/com.trmacs/timemachine-err.log"
+exec 1>> "${log}" 
+exec 2>> "${err_log}"
+
+when=$(date '+%m/%d/%Y %H:%M')
+
 home_check="10.0.240.10"
 subchck="$(nslookup home.benbass.com | grep 'Address' | tail -1 | cut -d : -f2 | awk '{print $1}')"
 time="$(date +%H)"
@@ -8,13 +15,13 @@ time="$(date +%H)"
 if [[ $time -lt 7 ]] || [[ $time -gt 17 ]]; then
 
 	if [[ $subchck == $home_check ]]; then
-		echo "On local network - and not between 7 and 5 - Running Time Machine..."
+		echo "At $when we are on the local network - and it is not between 7am and 5pm - Running Time Machine..."
 		tmutil startbackup --auto
 	else
-		echo "NOT on local network - Skipping Time Machine..."
+		echo "at $when we are NOT on local network - Skipping Time Machine..."
 	fi
 	else
-	echo "Skipping between 7am and 5pm "
+	echo "at $when it we are skipping time machine since it is between 7am and 5pm "
 
 fi
 	exit 0
