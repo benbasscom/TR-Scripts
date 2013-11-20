6fbe7de445d35f3bdd7bcf8e31ca0ab35331d11b
Ben Bass <ben@benbass.com>
2013-11-19 23:16:08 -0500

    time check update
    Cleaned up and modified when the script runs.  Now will skip at 6, 7, 8
    am.  Will run all other times.

--- Time Machine Schedule/Scripts/timemachine.sh
+++ Time Machine Schedule/Scripts/timemachine.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-#checks to see if home.benbass.com resolves to 10.0.240.10.  If not, skip running time machine.
+# Automatic Time Machine script
+vers="timemachine-1.4"
+# Created by Ben Bass
+# Copyright 2013 Technology Revealed. All rights reserved.
+# checks to see if home.benbass.com resolves to 10.0.240.10.  If not, skip running time machine.
+# does not run from 6:01 am to 8:59 am.  This is to prevent stopping a backup when leaving for work.
+# 1.3 added vers variable and changed skipping from 7 am to 5 pm to skipping 6:01 am to 8:59 am.
+# 1.3.1 Tweaked logging verbiage.
+# 1.4 cleaned up and tested 6, 7 and 8 cut off times - doing string comparison & not integer.
 
 log="/Library/Logs/com.trmacs/timemachine.log"
 err_log="/Library/Logs/com.trmacs/timemachine-err.log"
@@ -12,16 +20,16 @@ home_check="10.0.240.10"
 subchck="$(nslookup home.benbass.com | grep 'Address' | tail -1 | cut -d : -f2 | awk '{print $1}')"
 time="$(date +%H)"
 
-if [[ $time -lt 7 ]] || [[ $time -gt 17 ]]; then
-
-	if [[ $subchck == $home_check ]]; then
-		echo "At $when we are on the local network - and it is not between 7am and 5pm - Running Time Machine..."
-		tmutil startbackup --auto
-	else
-		echo "at $when we are NOT on local network - Skipping Time Machine..."
-	fi
-	else
-	echo "at $when it we are skipping time machine since it is between 7am and 5pm "
-
+if [ "$time" == "06" -o "$time" == "07" -o "$time" == "08" ]
+	then
+	echo "at $when, it is between 6 am to 9 am.  Skipping Time Machine."
+exit 0
 fi
-	exit 0
+
+if [[ $subchck == $home_check ]]; then
+	echo "At $when we are on the local network - and it is not between 6am and 9am - Running Time Machine..."
+	tmutil startbackup --auto
+else
+	echo "at $when we are NOT on local network - Skipping Time Machine..."
+fi
+exit 0
